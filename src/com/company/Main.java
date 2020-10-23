11ee5c1eb9ed8254d54bff9fed32bf0deecaa19e
Hammad Khan Musakhel <hammadmacho7@H4MM4D-machO7.local>
2020-10-24 02:02:10 +0500

    master: initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static int multiplication( int num1, int num2){
+        int mul = num1 * num2;
+        return mul;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int num1 = 4;
+        int num2 = 8;
+
+    }
+}
